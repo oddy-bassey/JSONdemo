08ca7b3236c4d7c99c63cdd803782ac40ad84f43
@revoltcode <oddybassey@gmail.com>
2020-12-02 03:20:50 +0100

    first commit

---
+++ app/src/main/java/com/example/jsondemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.jsondemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("STARTING-------------------------------------------");
+
+        Log.i("TESTING", "STARTING-------------------------------------------");
+
+        //DownloadTask downloadTask = new DownloadTask();
+        //downloadTask.execute("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02");
+    }
+}
